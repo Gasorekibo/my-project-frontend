78536a30dd7a8a409c890a29cedf9ea11fc2cede
Gasorekibo <m.gasore@alustudent.com>
2024-08-26 22:22:32 +0200

    new backed deployed api

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,4 +1,4 @@
-export const baseURL = "https://chw-backend.onrender.com";
-//
+export const baseURL = "https://chw-backend-ga9i.onrender.com";
+//https://chw-backend.onrender.com
 //http://localhost:5000
 //
